b12ac907ffc37f59e16a0e4db26654901658ee02
Nishant Chauhan <nishant031102@gmail.com>
2024-06-20 15:59:49 +0530

    Index file added

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const app = express();
 const PORT = 4042;
 const server = http.createServer(app);
 
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  res.sendFile("/public/index.html");
+});
+
 server.listen(PORT, (req, res) => {
   console.log(`Server started running on port number ${PORT}`);
 });
